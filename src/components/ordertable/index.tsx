b3634ffb97329220f25abad3a67eadc3e9d7b8a0
kangzeroo <kangzeroo@gmail.com>
2021-07-02 11:59:35 +0800

    tables are shown and styled

--- src/components/ordertable/index.tsx
+++ src/components/ordertable/index.tsx
@@ -1,3 +1,4 @@
+import { css } from "@emotion/css";
 import { TOrderRow } from "@/types/tickerFeed.type";
 
 interface IOrderTable {
@@ -6,26 +7,58 @@ interface IOrderTable {
 }
 const OrderTable = ({ title, rows }: IOrderTable) => {
   return (
-    <table>
-      <tr>
+    <table className={styles.table}>
+      <tr className={styles.title}>
         <th>{title}</th>
       </tr>
-      <tr>
-        <th>Price</th>
-        <th>Size</th>
-        <th>Total</th>
+      <tr className={styles.heading}>
+        <th className={styles.head}>Price</th>
+        <th className={styles.head}>Size</th>
+        <th className={styles.head}>Total</th>
       </tr>
       {rows.map((row) => {
         const { price, size, total } = row;
         return (
-          <tr>
-            <td>{price}</td>
-            <td>{size}</td>
-            <td>{total}</td>
+          <tr className={styles.row}>
+            <td className={styles.cell}>{price}</td>
+            <td className={styles.cell}>{size}</td>
+            <td className={styles.cell}>{total}</td>
           </tr>
         );
       })}
     </table>
   );
+};
+const styles = {
+  table: css`
+    color: white;
+    width: 100%;
+    text-align: right;
+    display: flex;
+    flex-direction: column;
+  `,
+  title: css`
+    display: flex;
+    flex-direction: column;
+  `,
+  heading: css`
+    font-weight: 400;
+    display: flex;
+  `,
+  head: css`
+    border-bottom: 1px solid white;
+    font-weight: 200;
+    width: 100%;
+  `,
+  row: css`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+  `,
+  cell: css`
+    text-align: right;
+    width: 100%;
+  `,
 };
 export default OrderTable;
